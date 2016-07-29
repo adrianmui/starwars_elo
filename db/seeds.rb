9a650c7b2c5441ad982eb8333ed9a75ed0abb62a
Adrian Mui <adrian.mui@gmail.com>
2016-07-28 20:26:34 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+a = Player.new()
+a.name = "adrian"
+a.height = 100
+a.mass = 100
+a.birth_year = "052695"
+a.gender = "male"
+a.homeworld = "Earth"
+a.save
